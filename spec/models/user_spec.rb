6e989475cee13f3bfcfbb6c0700473dd83aa54fb
jihaneh <jijih@hotmail.fr>
2022-05-24 11:27:03 +0200

    TDD : user generated by devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(email: 'test@test.com', password: 'helloWORLD') }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    user = User.new(email: nil)
+    expect(user).to_not be_valid
+  end
+  it 'is not valid without an password' do
+    user = User.new(password: nil)
+    expect(user).to_not be_valid
+  end
 end
