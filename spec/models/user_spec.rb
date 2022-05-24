6806e4dc5e8b8354933785d3f4031a8ce7211bc3
jihaneh <jijih@hotmail.fr>
2022-05-24 11:58:18 +0200

    TDD : add name to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { described_class.new(email: 'test@test.com', password: 'helloWORLD') }
+  subject { described_class.new(email: 'test@test.com', password: 'helloWORLD', name: 'test') }
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
   it 'is not valid without an email' do
-    user = User.new(email: nil)
-    expect(user).to_not be_valid
+    subject.email = nil
+    expect(subject).to_not be_valid
   end
+
   it 'is not valid without an password' do
-    user = User.new(password: nil)
-    expect(user).to_not be_valid
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without if name length is more than 30 characters' do
+    subject.name = 'a' * 40
+    expect(subject).to_not be_valid
   end
 end
