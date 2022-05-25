4c9a8238082d3556f6a6e11bf2c43cd1d12637ad
jihaneh <jijih@hotmail.fr>
2022-05-25 16:56:09 +0200

    TDD: Update tests: user not logged

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe '/categories', type: :request do
+RSpec.describe '/categories and user loggeg', type: :request do
   let(:user) { create(:user) }
   before { sign_in user }
   let(:valid_attributes) do
@@ -113,6 +113,106 @@ RSpec.describe '/categories', type: :request do
       category = Category.create! valid_attributes
       delete category_url(category)
       expect(response).to redirect_to(categories_url)
+    end
+  end
+end
+
+RSpec.describe '/categories and user not logged', type: :request do
+  let(:user) { create(:user) }
+  let(:valid_attributes) do
+    { name: 'Hobby', icon: 'hobby', user_id: user.id }
+  end
+
+  let(:invalid_attributes) do
+    { name: 'Hobby' }
+  end
+
+  describe 'GET /index' do
+    it 'redirects to the login page' do
+      Category.create! valid_attributes
+      get categories_url
+      expect(response).not_to be_successful
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'redirects to the login page' do
+      category = Category.create! valid_attributes
+      get category_url(category)
+      expect(response).not_to be_successful
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET /new' do
+    it 'redirects to the login page' do
+      get new_category_url
+      expect(response).not_to be_successful
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET /edit' do
+    it 'redirects to the login page' do
+      category = Category.create! valid_attributes
+      get edit_category_url(category)
+      expect(response).not_to be_successful
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'POST /create' do
+    context 'redirects to the login page' do
+      it 'creates a new Category' do
+        expect do
+          post categories_url, params: { category: valid_attributes }
+        end.to change(Category, :count).by(0)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'redirects to the login page' do
+        post categories_url, params: { category: valid_attributes }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        { name: 'Hobbies', icon: 'hobbies' }
+      end
+
+      it 'redirects to the login page' do
+        category = Category.create! valid_attributes
+        patch category_url(category), params: { category: new_attributes }
+        category.reload
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'redirects to the login page' do
+        category = Category.create! valid_attributes
+        patch category_url(category), params: { category: new_attributes }
+        category.reload
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'redirects to the login page' do
+      category = Category.create! valid_attributes
+      expect do
+        delete category_url(category)
+      end.to change(Category, :count).by(0)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'redirects to the login page' do
+      category = Category.create! valid_attributes
+      delete category_url(category)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
--- spec/requests/exchanges_spec.rb
+++ spec/requests/exchanges_spec.rb
@@ -117,3 +117,100 @@ RSpec.describe '/exchanges', type: :request do
     end
   end
 end
+
+RSpec.describe '/exchanges and user not logged', type: :request do
+  let(:user) { create(:user) }
+
+  let(:valid_attributes) do
+    { name: 'C&A', amount: 29.99, author_id: user.id }
+  end
+
+  let(:invalid_attributes) do
+    { name: 'C&A' }
+  end
+
+  describe 'GET /index' do
+    it 'redirects to the login page' do
+      Exchange.create! valid_attributes
+      get exchanges_url
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'redirects to the login page' do
+      exchange = Exchange.create! valid_attributes
+      get exchange_url(exchange)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET /new' do
+    it 'redirects to the login page' do
+      get new_exchange_url
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET /edit' do
+    it 'redirects to the login page' do
+      exchange = Exchange.create! valid_attributes
+      get edit_exchange_url(exchange)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'redirects to the login page' do
+        expect do
+          post exchanges_url, params: { exchange: valid_attributes }
+        end.to change(Exchange, :count).by(0)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'redirects to the login page' do
+        post exchanges_url, params: { exchange: valid_attributes }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        { name: 'C&A', amount: 129.99 }
+      end
+
+      it 'redirects to the login page' do
+        exchange = Exchange.create! valid_attributes
+        patch exchange_url(exchange), params: { exchange: new_attributes }
+        exchange.reload
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'redirects to the login page' do
+        exchange = Exchange.create! valid_attributes
+        patch exchange_url(exchange), params: { exchange: new_attributes }
+        exchange.reload
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'redirects to the login page' do
+      exchange = Exchange.create! valid_attributes
+      expect do
+        delete exchange_url(exchange)
+      end.to change(Exchange, :count).by(0)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it 'redirects to the login page' do
+      exchange = Exchange.create! valid_attributes
+      delete exchange_url(exchange)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
