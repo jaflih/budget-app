3c5a6c5b0f146f52112dc11b34cdc027407a34e9
jihaneh <jijih@hotmail.fr>
2022-05-25 10:19:58 +0200

    TDD exchange controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :exchanges
+  resources :slots
   resources :categories
   devise_for :users, :controllers => { registrations: 'users/registrations' }
 
