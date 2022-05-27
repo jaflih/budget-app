596e92bd277690cd226939c7d3d78c4b1b835966
jihaneh <jijih@hotmail.fr>
2022-05-27 15:47:48 +0200

    Fix sign-up error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   root "home#home"
+  get 'users', to: "home#home"
 
   resources :categories, only: [:index, :new, :create]  do
     resources :exchanges, only: [:index, :new, :create]
